723509f9aad53502fa04cc78117011ae3f0597fd
Andrew Walser <awalser@Andrews-Mac-mini.local>
2017-01-12 12:41:03 -0700

    First commit

--- PercentageCalculatorTests/PercentageCalculatorTests.swift
+++ PercentageCalculatorTests/PercentageCalculatorTests.swift
@@ -11,11 +11,31 @@ import XCTest
 
 class PercentageCalculatorTests: XCTestCase {
     
+    var vc: ViewController!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+        vc = storyboard.instantiateInitialViewController() as! ViewController
     }
     
+    func testPercentageCalculator() {
+        let p = vc.percentage(50, 50)
+        XCTAssert(p == 25)
+        
+    }
+    
+    
+    func testLabelValuesShowedProperly() {
+        let _ = vc.view
+        vc.updateLabels(Float(80.0), Float(50.0), Float(40.0))
+        
+        // The labels should now display 80, 50 and 40
+        XCTAssert(vc.numberLabel.text == "80.0", "numberLabel doesn't show the right text")
+        XCTAssert(vc.percentageLabel.text == "50.0%", "percentageLabel doesn't show the right text")
+       XCTAssert(vc.resultLabel.text == "40.0", "resultLabel doesn't show the right text")
+    }
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
